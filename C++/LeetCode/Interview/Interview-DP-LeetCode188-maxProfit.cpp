384f10dd39ef3dbcd9d38f28e11ddd793b429c72
orekiousun <1187749429@qq.com>
2024-03-09 12:24:05 +0800

    【2024.03.09】买卖股票最佳时机（还没看完）

--- C++/LeetCode/Interview/Interview-DP-LeetCode188-maxProfit.cpp
+++ C++/LeetCode/Interview/Interview-DP-LeetCode188-maxProfit.cpp
@@ -12,58 +12,12 @@ using namespace std;
  */
 
 int maxProfit(int k, vector<int>& prices) {
-    int size = prices.size();
 
-    // dp[i][j]表示第i天买入，第j天前卖出获得的最大收益
-    // dp[0][k]记录第k天前卖出时所能获得的最大收益
-    vector<vector<int>> dp(size + 1, vector<int>(size + 1));
-    vector<int> stackCnt(size + 1, 0);
-    int res = 0;
-    for (int i = 1; i <= size; ++i) {
-        for (int j = i; j <= size; ++j) {
-            dp[i][j] = max(dp[i][j - 1], prices[j - 1] - prices[i - 1]);
-            if (dp[0][i - 1] + dp[i][j] > dp[0][j]) {
-                // 记录买卖次数
-                stackCnt[j] = stackCnt[i - 1] + 1;
-                dp[0][j] = dp[0][i - 1] + dp[i][j];
-                if (stackCnt[j] <= k)
-                    res = max(res, dp[0][j]);
-            }
-//            dp[0][j] = max(dp[0][i - 1] + dp[i][j], dp[0][j]);
-        }
-    }
-    return res;
-}
-
-// 内存优化 --
-int maxProfit2(int k, vector<int>& prices) {
-    int size = prices.size();
-
-    // dp[i][j]表示第i天买入，第j天前卖出获得的最大收益
-    vector<int> dp(size + 1, 0);
-    // maxProfitBefore[k]记录第k天前卖出时所能获得的最大收益
-    vector<int> maxProfitBefore(size + 1, 0);
-    vector<int> stackCnt(size + 1, 0);
-    int res = 0;
-    for (int i = 1; i <= size; ++i) {
-        for (int j = i; j <= size; ++j) {
-            dp[j] = max(dp[j - 1], prices[j - 1] - prices[i - 1]);
-            if (maxProfitBefore[i - 1] + dp[j] > maxProfitBefore[j]) {
-                // 记录买卖次数
-                stackCnt[j] = stackCnt[i - 1] + 1;
-                maxProfitBefore[j] = maxProfitBefore[i - 1] + dp[j];
-                if (stackCnt[j] <= k)
-                    res = max(res, maxProfitBefore[j]);
-            }
-//            dp[0][j] = max(dp[0][i - 1] + dp[i][j], dp[0][j]);
-        }
-    }
-    return res;
 }
 
 
 int main() {
-    vector<int> prices{1, 4, 0, 3, 1, 4};
-    cout << maxProfit(2, prices) << endl;
+    vector<int> prices{1,4,7,5,6,2,5,1,9};
+    cout << maxProfit(3, prices) << endl;
     return 0;
 }
