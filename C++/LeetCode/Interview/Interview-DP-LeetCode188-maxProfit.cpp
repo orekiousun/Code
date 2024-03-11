25d5f7966f10ca80af04c5eff820f5a3660b37e0
orekiousun <1187749429@qq.com>
2024-03-11 10:27:44 +0800

    【2024.03.11】买卖股票的最佳时机

--- C++/LeetCode/Interview/Interview-DP-LeetCode188-maxProfit.cpp
+++ C++/LeetCode/Interview/Interview-DP-LeetCode188-maxProfit.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <algorithm>
 
 /*
     给你一个整数数组 prices 和一个整数 k ，其中 prices[i] 是某支给定的股票在第 i 天的价格。
     设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。
     注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
 
-    二维动态规划dp[i][j]表示在i处买入，j之前卖出获得的最大收益
-    dp[i][j] = max(dp[i][j - 1], prices[j] - prices[i])
+    buy[i][j]: 恰好进行j笔交易，并且当前手上持有一支股票，这种情况下的最大利润
+    sell[i][j]: 恰好进行j笔交易，并且当前手上不持有股票，这种情况下的最大利润
+    状态转移方程：
+        buy[i][j]
+        -- 第i天买入：buy[i][j] = sell[i-1][j] - prices[i]
+        -- 不是第i天买入：buy[i][j] = buy[i - 1][j]
+        sell[i][j]
+        -- 第i天卖出：buy[i-1][j-1] + prices[i]
+        -- 不是第i天卖出：sell[i-1][j]
  */
 
 int maxProfit(int k, vector<int>& prices) {
+    if (prices.empty())
+        return 0;
 
+    int n = prices.size();
+    k = min(k, n / 2);
+    vector<vector<int>> buy(n, vector<int>(k + 1));
+    vector<vector<int>> sell(n, vector<int>(k + 1));
+
+    // 初始化
+    buy[0][0] = -prices[0];
+    sell[0][0] = 0;
+    for (int i = 1; i <= k ; ++i) {
+        buy[0][i] = sell[0][i] = INT_MIN / 2;
+    }
+
+    for (int i = 1; i < n; ++i) {
+        buy[i][0] = max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+        for (int j = 1; j <= k; ++j) {
+            buy[i][j] = max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+            sell[i][j] = max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+        }
+    }
+
+    return *max_element(sell[n - 1].begin(), sell[n - 1].end());
 }
 
 
--- C++/LeetCode/Test.cpp
+++ C++/LeetCode/Test.cpp
@@ -2,21 +2,60 @@
 using namespace std;
 #include<vector>
 
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
 
-void BubbleSort(vector<int>& nums) {
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums.size() - i - 1; j++) {
-            if(nums[j] > nums[j + 1]) {
-                int temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
+void DFS(TreeNode* node, int curVal, int& maxVal, vector<int> curPath, vector<int>& maxPath) {
+    curPath.push_back(node->val);
+    curVal += node->val;
+    if (node->left == nullptr && node->right == nullptr) {
+        if (curVal > maxVal) {
+            maxPath = curPath;
+            maxVal = curVal;
         }
+    }
+
+    if (node->left != nullptr) {
+        DFS(node->left, curVal, maxVal, curPath, maxPath);
+    }
+    if (node->right != nullptr) {
+        DFS(node->right, curVal, maxVal, curPath, maxPath);
     }
 }
 
-int main(int argc, char const *argv[]) {
-    vector<int> nums{3, 4, 5, 1, 2};
-    BubbleSort(nums);
+vector<int> maxPath(TreeNode* root) {
+    vector<int> res;
+    vector<int> curPath;
+    if (root == nullptr)
+        return res;
+
+    int maxVal = INT_MIN;
+    DFS(root, 0, maxVal, curPath, res);
+    return res;
+}
+
+int main() {
+
+    TreeNode* l1 = new TreeNode(2);
+    TreeNode* l2 = new TreeNode(5);
+    TreeNode* l3 = new TreeNode(6);
+    TreeNode* l4 = new TreeNode(-1);
+    TreeNode* l5 = new TreeNode(3);
+    TreeNode* l6 = new TreeNode(-5);
+
+    TreeNode* root = new TreeNode(4);
+    root->left = l4;
+    l4->left = l1;
+    l4->right = l2;
+    l1->left = l5;
+    l1->right = l3;
+    l3->left = l6;
+    vector<int> res = maxPath(root);
+
     return 0;
 }
